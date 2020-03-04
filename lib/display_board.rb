b80b150c42d88a38661ded79cfc943398f4650be
mathematical-integer-7084 <shawnaejohnson6@gmail.com>
2020-03-04 16:43:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,3 +9,13 @@ def display_board(board)
   puts "   |   |   "
 end
 display_board(board)
+
+board = [" "," "," "," ","X"," "," "," "," "]
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+display_board(board)
