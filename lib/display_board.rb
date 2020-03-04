8befa923314696ad5ef81117d6f3c2ffbcae59e3
mathematical-integer-7084 <shawnaejohnson6@gmail.com>
2020-03-04 16:41:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
-def display_board 
+def display_board
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
@@ -10,5 +10,3 @@ def display_board
 end
 
 display_board(board)
-
-
