f4e2bc3ad3d9046dc22a5a81c0df9a3e0fa360e7
mathematical-integer-7084 <shawnaejohnson6@gmail.com>
2020-03-04 16:34:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["O"," "," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-  puts "   |   |   "
+  puts " O |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   | X |   "
   puts "-----------"
   puts "   |   |   "
 end
